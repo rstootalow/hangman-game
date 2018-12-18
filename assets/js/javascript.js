9ee2f4d82d97208bcb6da48f0522d34285214d43
rstutalo <richardtutalo@gmail.com>
2018-12-17 22:38:35 -0700

    basic layout and theme complete - no style or logic in place

---
+++ assets/js/javascript.js
@@ -0,0 +1,15 @@
+/* PSEUDOCODE */
+
+/* 
+1) User presses any key to start the game (code document key up so any key will start the game.)
+2) Randomly choose a word for the user to guess
+3) if user correctly guesses letter:
+    - that letter should display in in a text box.
+    - 
+
+
+*/
+
+/* START OF GAME */
+
+// alert("Press Any Key to Get Started!");
