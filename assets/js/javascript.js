23d435c8533e86758f97df4bf7cc923fe2003abf
rstutalo <richardtutalo@gmail.com>
2019-01-03 19:51:18 -0700

    code for wrong letters now populating in incorrect guess segment

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,68 +1,67 @@
-// GLOBAL VARIABLES
+
 
 // WORD BANK OF WORDS TO RANOMLY CHOOSE FROM
-gameWords = ["tool", "metallica", "megadeth", "nirvana", "biggie"];
+gameWords = ["tool", "metallica", "megadeth", "nirvana", "biggie", "snoop", "live"];
 
 var selectedWord; // holds the selected word from the word bank array
-var lettersInWord = [];
-// var correctGuesses = []; // variable to store correct LETTER guesses from the player. START with "_" then poulate with correct key presses
-var wrongLetters = []; // array to hold the incorrect LETTER guesses from the player
+var lettersInWord = []; 
+var wrongGuess = []; // array to hold the incorrect LETTER guesses from the player
 var wins = 0; // counter for number of WORDS guessed correctly
 var losses = 0; // counter for number of WORDS guessed incorrectly
 var numOfGuessesLeft = 10; // number of guesses remaining on current word
-var underscoresAndGuesses  = []; // variable that keeps count of the number of words in selected word and puts an underscore there. 
+var underscoresAndGuesses  = []; // variable that keeps count of the number of words in selected word with "_" and correct guesses
 var userGuess; // the key press the user makes when selecting the words. 
 
 
+// choose a random word from the word bank for the player to guess
+selectedWord = gameWords[Math.floor(Math.random() * gameWords.length)].toString();
+// get list of letters in selectedWord and add those letters to the lettersInWord array
+lettersInWord = selectedWord.split("");
+console.log(lettersInWord); // TEST
 
-
-
-
-/* FUNCTIONS TO BE USED IN THE GAME */
-
-// alert("Press any key to start the game!");
+document.getElementById("bandToGuess").innerHTML = "Band To Guess:  " + "<br>" + underscoresAndGuesses.join(' ');
+document.getElementById("incorrect-guesses").innerHTML = "Incorrect Guesses: " + "<br>" + wrongGuess.join(' ');
+document.getElementById("guesses-left").innerHTML = "Guesses Left: " + numOfGuessesLeft;
+document.getElementById("wins").innerHTML = "Wins: " + wins;
+document.getElementById("losses").innerHTML = "Losses: " + losses;
 
 // START OF THE GAME
+function gameStart() {
+    // populate selected word with "_" based on length of selected word
+    for(var i = 0; i < selectedWord.length; i++) {
+            underscoresAndGuesses[i] = " _ "; //adds underscore to the bandToGuess field to indicate how many letters are in the word.
     
-        function gameStart() {
-            // choose a random word from the word bank for the player to guess
-            selectedWord = gameWords[Math.floor(Math.random() * gameWords.length)].toString();
-            //TEST
-            console.log(selectedWord); // TEST
-            // get list of letters in selectedWord and add those letters to the lettersInWord array
-            lettersInWord = selectedWord.split("");
-            console.log(lettersInWord); // TEST
-            // populate selected word with "_" based on length of selected word
-            for(var i = 0; i < selectedWord.length; i++) {
-                    underscoresAndGuesses[i] = " _ "; //adds underscore to the bandToGuess field to indicate how many letters are in the word. 
-                    
-            }
-
-
-            //key presses to start populating letters
-            document.onkeyup = function(event) {
-                userGuess = event.key; // key press value is added to userGuess variable
-                // console.log(userGuess); // TEST
-                // if single userGuess key press = one of the letters of the selected word, add that letter to correct guess and in the correct position
-                for (var i = 0; i < lettersInWord.length; i++) {
-                        if(lettersInWord[i] === userGuess) { // if the letter in the current iteration === the userGuess (key press) 
-                            underscoresAndGuesses[i] = userGuess;
-                            document.getElementById("band-to-guess").innerHTML = underscoresAndGuesses.join(" ");
-                        }
-                            
-                    }
+    }
+    /* DEFAULT HTML ELEMENTS TO POPULATE AT START OF GAME */
+    document.getElementById("bandToGuess").innerHTML = "Band To Guess:  " + "<br>" + underscoresAndGuesses.join(' ');
+    document.getElementById("incorrect-guesses").innerHTML = "Incorrect Guesses: " + "<br>" + wrongGuess.join(' ');
+    document.getElementById("guesses-left").innerHTML = "Guesses Left: " + numOfGuessesLeft;
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+}
+function checkLetter() {
+     //key presses to start choose letters
+     document.onkeypress = function(event) {
+        userGuess = event.key // key press value is added to userGuess variable
+        var letterFound = false;
+        for (var i = 0; i < lettersInWord.length; i++) {
+                    if(lettersInWord[i] === userGuess) {
+                        underscoresAndGuesses[i] = userGuess;
+                        document.getElementById("bandToGuess").innerHTML = "Band To Guess: " + "<br>" + underscoresAndGuesses.join("");
+                        letterFound = true;
+                    } 
                 }
+             if (letterFound) return;
+                
                 // if the letter is not one of the letters in selected word
+                if (wrongGuess.indexOf(userGuess < 0)) {
+                        wrongGuess.push(userGuess);
+                        console.log(wrongGuess); // TEST
+                        document.getElementById("incorrect-guesses").innerHTML = "Incorrect Guesses: " + "<br>" + wrongGuess.join(" ");
+                    }
 
-                /* Javascript logic added to HTML elements */
-            document.getElementById("band-to-guess").innerHTML = "Band To Guess:  " + "<br>" + underscoresAndGuesses.join(' ');
-            document.getElementById("incorrect-guesses").innerHTML = "Incorrect Guesses " + "<br>" + wrongLetters.join(' ');
-            // document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + numOfGuessesLeft; 
-
-            }
-
-        
-        /* FUNCTION TO CHECK IF ALL LETTERS HAVE BEEN GUESSED IN SELECTED WORD */
+                }
+}
 
         /* Game End and Reset Code Goes Here */
 
@@ -72,6 +71,7 @@ var userGuess; // the key press the user makes when selecting the words.
 
 // RUN FUNCTIONS
 gameStart();
+checkLetter();
 
 
 
